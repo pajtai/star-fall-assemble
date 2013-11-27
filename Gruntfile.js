54bfbf0d1b1b2fbf51728a9db32d55b7485fc0f8
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-11-26 22:38:36 -0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,6 +91,26 @@ module.exports = function (grunt) {
                 jshintrc : '.jshintrc'
             }
         },
+
+        requirejs: {
+            dist: {
+                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                options: {
+                    // `name` and `out` is set by grunt-usemin
+                    baseUrl: 'app',
+                    optimize: 'none',
+                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
+                    // https://github.com/yeoman/grunt-usemin/issues/30
+                    //generateSourceMaps: true,
+                    // required to support SourceMaps
+                    // http://requirejs.org/docs/errors.html#sourcemapcomments
+                    preserveLicenseComments: false,
+                    useStrict: true,
+                    wrap: true
+                    //uglify2: {} // https://github.com/mishoo/UglifyJS2
+                }
+            }
+        }
     });
 
     // To start editing your slideshow using livereload, run 'grunt server'
